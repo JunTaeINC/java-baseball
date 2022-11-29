96ecd8c5317341ca4158647decd873e7c7179541
JunTaeINC <juntea3523@naver.com>
2022-11-29 16:09:39 +0900

    feat : (Validator) stringCheck 기능 구현

--- src/main/java/baseball/utility/Validator.java
+++ src/main/java/baseball/utility/Validator.java
@@ -1,12 +1,14 @@
 package baseball.utility;
 
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class Validator {
 
     private static final int MIN_RANGE = 1;
     private static final int MAX_RANGE = 9;
-    private final int MAX_LENGTH = 3;
+    private static final int MAX_LENGTH = 3;
+    private static final String ZERO_TO_NINE = "^[0-9]*$";
 
     public static boolean checkNumberRangeCheck(List<Integer> testObject) {
         for (Integer number : testObject) {
@@ -19,6 +21,14 @@ public class Validator {
 
     public static boolean numberLengthCheck(String testObject) {
         if (testObject.length() != 3) {
+            throw new IllegalArgumentException();
+        }
+        return true;
+    }
+
+    public static boolean stringCheck(String testObject) {
+        Pattern zeroToNine = Pattern.compile(ZERO_TO_NINE);
+        if (!zeroToNine.matcher(testObject).matches()) {
             throw new IllegalArgumentException();
         }
         return true;
