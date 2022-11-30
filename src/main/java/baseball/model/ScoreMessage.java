2c7fa62010dd52edac60e116feadd9388f1751a7
JunTaeINC <juntea3523@naver.com>
2022-11-30 09:48:31 +0900

    feat : (ScoreMessage) enum 생성

---
+++ src/main/java/baseball/model/ScoreMessage.java
@@ -0,0 +1,13 @@
+package baseball.model;
+
+public enum ScoreMessage {
+    STRIKE("스트라이크"),
+    BALL("볼"),
+    NOTHING("낫싱");
+
+    private final String scoreMessage;
+
+    ScoreMessage(final String scoreMessage){
+        this.scoreMessage = scoreMessage;
+    }
+}
