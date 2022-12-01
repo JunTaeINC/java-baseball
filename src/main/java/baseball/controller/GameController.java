0e8bebcd9195240b7e698ee35c47bf7aef13103b
JunTaeINC <juntea3523@naver.com>
2022-12-01 15:05:54 +0900

    chore : (OutputView) printResult 기능 분리

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -4,31 +4,34 @@ import baseball.model.ComputerNumber;
 import baseball.model.UserNumber;
 import baseball.model.ScoreMessage;
 import baseball.view.InputView;
+import baseball.view.OutputView;
 
 
 import java.util.List;
 import java.util.Objects;
 
 public class GameController {
+    ComputerNumber computerNumber = new ComputerNumber();
+    UserNumber userNumber = new UserNumber();
+    InputView inputView = new InputView();
+    OutputView outputView = new OutputView();
 
     private final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
     private final String END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     private final String RESTART = "1";
-    private final String BLANK = " ";
-
-
-    ComputerNumber computerNumber = new ComputerNumber();
-    UserNumber userNumber = new UserNumber();
-    InputView inputView = new InputView();
+    private final int ZERO = 0;
+    private final int THREE = 3;
+    private boolean exitCode = false;
+    private int strike = 0;
+    private int ball = 0;
     private List<Integer> computerNumbersList = computerNumber.setComputerNumbers();
 
     public void playGame() {
         System.out.println(START_MESSAGE);
-        boolean exitCode = false;
         while (!exitCode) {
             List<Integer> userNumbersList = userNumber.setUserNumbers(inputView.getUserInput());
-            System.out.println(printResult(computerNumbersList, userNumbersList));
-            if (countStrike(computerNumbersList, userNumbersList) == 3) {
+            System.out.println(outputView.printResult(strike,ball));
+            if (countStrike(computerNumbersList, userNumbersList) == THREE) {
                 exitCode = exitOrRestart(inputView.getExitOrRestart());
             }
         }
@@ -44,24 +47,8 @@ public class GameController {
         }
     }
 
-    private String printResult(List<Integer> computer, List<Integer> user) {
-        int strike = countStrike(computer, user);
-        int ball = countBall(computer, user);
-        if (strike == 0 && ball == 0) {
-            return ScoreMessage.NOTHING.getScoreMessage();
-        }
-        if (strike == 0) {
-            return ball + ScoreMessage.BALL.getScoreMessage();
-        } else if (ball == 0) {
-            return strike + ScoreMessage.STRIKE.getScoreMessage();
-        } else {
-            return ball + ScoreMessage.BALL.getScoreMessage() + BLANK
-                    + strike + ScoreMessage.STRIKE.getScoreMessage();
-        }
-    }
-
     public int countStrike(List<Integer> computer, List<Integer> user) {
-        int strike = 0;
+        int strike = ZERO;
         for (int space = 0; space < computer.size(); space++) {
             if (Objects.equals(computer.get(space), user.get(space))) {
                 strike++;
@@ -71,7 +58,7 @@ public class GameController {
     }
 
     public int countBall(List<Integer> computer, List<Integer> user) {
-        int ball = 0;
+        int ball = ZERO;
         for (int space = 0; space < computer.size(); space++) {
             if (!Objects.equals(computer.get(space), user.get(space)) && computer.contains(user.get(space))) {
                 ball++;
---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,23 @@
+package baseball.view;
+
+import baseball.model.ScoreMessage;
+
+public class OutputView {
+
+    private final int ZERO = 0;
+    private final String BLANK = " ";
+
+    public String printResult(int strike, int ball) {
+        if (strike == ZERO && ball == ZERO) {
+            return ScoreMessage.NOTHING.getScoreMessage();
+        }
+        if (strike == ZERO) {
+            return ball + ScoreMessage.BALL.getScoreMessage();
+        } else if (ball == ZERO) {
+            return strike + ScoreMessage.STRIKE.getScoreMessage();
+        } else {
+            return ball + ScoreMessage.BALL.getScoreMessage() + BLANK
+                    + strike + ScoreMessage.STRIKE.getScoreMessage();
+        }
+    }
+}
